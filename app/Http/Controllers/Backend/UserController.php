2d1d2f3755eb6a59886ac6818572c92006cbab26
Emrah Yurtlu <emrahyurtlu@gmail.com>
2022-01-23 22:26:31 +0300

    UserController.php is optimized.

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-use function Symfony\Component\String\u;
 
 class UserController extends Controller
 {
